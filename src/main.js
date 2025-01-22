ec33a3226d58c1aa304ce794c2a1ca583dc5ceda
mudroljub <mudroljub@gmail.com>
2025-01-22 10:45:00 +0100

    izbačen css

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Sprite from './Sprite.js'
 import { elements } from './data.js'
-import { ctx } from './platno.js'
+import { platno, ctx } from './platno.js'
 
-const mountains = document.getElementById('sky-box')
+const bgImage = new Image()
+bgImage.src = 'slike/planine.png'
 
 const totalImages = elements.reduce((acc, el) => acc + el.urls.length, 0)
 const sprites = []
@@ -38,9 +39,20 @@ function loop() {
   requestAnimationFrame(loop)
 
   ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
-  worldRot += dWorldRot
+  worldRot += dWorldRot * 10
 
-  mountains.style.backgroundPosition = -(worldRot / Math.PI * 2) * 100 + '%'
+  const scaledWidth = window.innerWidth * 2
+  const scaledHeight = window.innerHeight * 0.4
+  const bgOffsetX = (worldRot / Math.PI * 2) * 100 % scaledWidth
+
+  ctx.fillStyle = '#403'
+  ctx.fillRect(0, 0, platno.width, 300)
+
+  ctx.fillStyle = '#030'
+  ctx.fillRect(0, 300, platno.width, platno.height)
+
+  for (let x = bgOffsetX - scaledWidth; x < window.innerWidth; x += scaledWidth)
+    ctx.drawImage(bgImage, x, 100, scaledWidth, scaledHeight)
 
   sprites
     .sort((a, b) => b.position.z - a.position.z)
